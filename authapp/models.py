6abdaad265814dea13b6bc5b32f853f914b6c4f5
Denis <Denis.Gorshkov76@gmail.com>
2019-03-03 23:09:31 +0300

    Добавил страницу регистрации Добавил страницу редактирования Добавил отображение аватарки Добавил стандартную аватарку Стилизовал формы(регистрации,редактирования,входа)

--- authapp/models.py
+++ authapp/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class ShopUser(AbstractUser):
-    avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name = 'возраст')
+    avatar = models.ImageField(upload_to='media/users_avatars', blank=True, default='../static/img/no_avatar.png')
+    age = models.PositiveIntegerField(verbose_name='возраст')
