742651be8306ea141524b020e0b7d96f9bfdb86a
Denis <Denis.Gorshkov76@gmail.com>
2019-03-15 00:39:35 +0300

    Выполни пункты 7 задания Создал форму отправки сообщений(обратная связь) Поработал со встроенной админкой

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -127,3 +127,10 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_URL = '/auth/login/'
+# EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com' #Например, smtp.gmail.com
+EMAIL_HOST_USER = 'Denis@gmail.com' #Например, user@gmail.com. Именно его необходимо указывать как ВАШ_EMAIL_ДЛЯ_ОТПРАВКИ_СООБЩЕНИЯ в исходном коде предыдущего пункта
+EMAIL_HOST_PASSWORD = '*****'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
